a646dd3d356e26963b989c0b7baace5a95e3dde1
zzhu2011 <zzhu1987@gmail.com>
2014-03-28 16:57:57 -0700

    fixed a small bug so that getExpr can work properly

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ZzCompiler
 {
@@ -12,6 +13,20 @@ namespace ZzCompiler
         //task 1: implement the regexp for string and () and empty, write the unite test for them
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.TestGetTokenNameAndExpr();
+        }
+
+        void TestGetTokenNameAndExpr()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@".\TestData\TokenNameAndExpr.txt");
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string tokenName = null;
+                string expr = null;
+                StateMachine.getExpr(out tokenName, out expr, lines[i], i + 1);
+                Console.WriteLine("TokenName:{0} Expr:{1}", tokenName, expr);
+            }
         }
     }
 }
--- StateMachine.cs
+++ StateMachine.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace ZzCompiler
 {
 
-    abstract class StateMachine
+    class StateMachine
     {
         enum EdgeLabel { EMPTY = -3, CCL = -2, EPSILON = -1 };
         class NFAState 
@@ -31,22 +31,15 @@ namespace ZzCompiler
             }
         }
 
-        static Regex TokenPattern = new Regex(@"^TOKENNAME:(\w+) EXPRESSION:(.*)( //.*)?$");
-        static Regex CommentPattern = new Regex(@"^//.*$");
+        static Regex TokenPattern = new Regex(@"(^(TOKENNAME:\s*(?<TokenName>\w+)\s+EXPRESSION:\s*(?<Expr>.*)\s*)$)|(^\s*(\/\/.*)?$)", RegexOptions.IgnoreCase);
+        static Regex CommentPattern = new Regex(@"^\/\/.*$");
         static Regex EmptyLinePattern = new Regex(@"^[ ]*$");
         public static void getExpr(out string TokenName, out string Expr, string inputLine, int LineNum)
         {
-            MatchCollection mc = TokenPattern.Matches(inputLine);
-            string []strs = null;
-            TokenName = null;
-            Expr = null; 
-            if (mc.Count > 0)
-            {
-                strs = mc.OfType<Match>().Select(m => m.Value).ToArray();
-                TokenName = strs[0];
-                Expr = strs[1];
-            }
-            else if (!CommentPattern.Match(inputLine).Success&&!EmptyLinePattern.Match(inputLine).Success)
+            Match mc = TokenPattern.Match(inputLine);
+            TokenName = mc.Groups["TokenName"].Value;
+            Expr = mc.Groups["expr"].Value;
+            if(!mc.Success)
             {
                 Console.Error.WriteLine("input formate wrong in Line: " + LineNum);
             }
