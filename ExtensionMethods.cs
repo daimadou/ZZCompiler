657cff5390dc7d09500dcff8e715d8186947bcdc
zzhu2011 <zzhu1987@gmail.com>
2014-04-25 18:34:00 -0700

    move extension method into different files

---
+++ ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    using ZzCompiler;
+    static class ExtensionMethods
+    {
+        public static DFAState IsNFASetExisted(this List<DFAState> DFASet, HashSet<NFAState> NFASet)
+        {
+            DFAState ret = null;
+            foreach (var s in DFASet)
+            {
+                if (s.NfaStatesSet.SetEquals(NFASet))
+                {
+                    ret = s;
+                    break;
+                }
+            }
+            return ret;
+        }
+    }
+}
